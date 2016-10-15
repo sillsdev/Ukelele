df1234d34112861233e5b4449b5a48ef68d1dd72
John Brownie <john_brownie@sil.org>
2016-10-15 12:58:00 +0300

    Some more steps towards localisation

---
+++ Ukelele Cocoa/LocaleCode.h
@@ -0,0 +1,13 @@
+//
+//  LocaleCode.h
+//  Ukelele
+//
+//  Created by John Brownie on 14/10/16.
+//  Copyright © 2016 John Brownie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocaleCode : NSObject
+
+@end
---
+++ Ukelele Cocoa/LocaleCode.mm
@@ -0,0 +1,13 @@
+//
+//  LocaleCode.m
+//  Ukelele
+//
+//  Created by John Brownie on 14/10/16.
+//  Copyright © 2016 John Brownie. All rights reserved.
+//
+
+#import "LocaleCode.h"
+
+@implementation LocaleCode
+
+@end
